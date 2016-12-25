056cb366463f2805d1be967a4924580e864fa1b1
cneubauern <cneubauern@googlemail.com>
2016-12-25 18:19:57 +0100

    static template

--- client/main.ts
+++ client/main.ts
@@ -1,4 +1,4 @@
-import 'angular-meteor-polyfills';
+import 'angular2-meteor-polyfills';
 import {platformBrowserDynamic} from "@angular/platform-browser-dynamic";
 import AppModule from "./imports/app/app.module";
 
